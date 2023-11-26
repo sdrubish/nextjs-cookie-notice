01e5ed84afe2d15731f34d2b741e093eaefa7cc1
DruuuCLT <4332199+sdrubish@users.noreply.github.com>
2023-11-26 12:17:17 +0100

    Init

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<main className="">
+			<div className="druuu-1 druuu-gradient"></div>
+
+			<div>
+				<h1 className="logo">Drulab</h1>
+				<h2 className="sublogo">Tailored coding + Blockchain + AI + Web + Business Automation</h2>
+				<h3 className="main-text">Home to everything that's food for the mind in the tech space.</h3>
+			</div>
+		</main>
+	);
+}
